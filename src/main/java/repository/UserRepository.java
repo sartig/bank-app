1101a70b9a210a973ee5638c8ce7ec80ce300515
soonyang thiang <soonyang.thiang@fdmgroup.com>
2024-02-26 15:56:57 +0800

    Implemented UserService, UserRepository, UserController and RegistrationController

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,19 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.User;
+import repository.UserRepository;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void registerUser(User user) {
+		// validation and logic here
+		userRepository.save(user);
+	}
 }
