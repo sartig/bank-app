08db43f06bacc0b6f4d7df02f0c7dc6a4f11f854
ashwinSarith <ash.sarith@fdmgroup.com>
2024-02-26 15:56:22 +0800

    set up index page

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -9,17 +9,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
-
     private String username;
     private String password;
-    private String email;
     private String firstName;
     private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String country;
     private String phone;
     private String creditCardNumber;
     private String creditCardExpiration;
@@ -29,22 +22,25 @@ public class User {
         super();
     }
 
-    public User(String username, String password, String email, String firstName, String lastName, String address, String city, String state, String zip, String country, String phone, String creditCardNumber, String creditCardExpiration, String creditCardCVV) {
-        super();
+    public User(Long userId, String username, String password, String firstName, String lastName, String phone, String creditCardNumber, String creditCardExpiration, String creditCardCVV) {
+        this.userId = userId;
         this.username = username;
         this.password = password;
-        this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.country = country;
         this.phone = phone;
         this.creditCardNumber = creditCardNumber;
         this.creditCardExpiration = creditCardExpiration;
         this.creditCardCVV = creditCardCVV;
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public String getUsername() {
@@ -63,14 +59,6 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getFirstName() {
         return firstName;
     }
@@ -87,60 +75,37 @@ public class User {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-
-    public String getZip() {
-        return zip;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
     public String getPhone() {
         return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getCreditCardNumber() {
         return creditCardNumber;
     }
 
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
     public String getCreditCardExpiration() {
         return creditCardExpiration;
+    }
+
+    public void setCreditCardExpiration(String creditCardExpiration) {
+        this.creditCardExpiration = creditCardExpiration;
     }
 
     public String getCreditCardCVV() {
         return creditCardCVV;
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
+    public void setCreditCardCVV(String creditCardCVV) {
+        this.creditCardCVV = creditCardCVV;
     }
 }
+
+
