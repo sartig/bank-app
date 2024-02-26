4627fd60c3f811a9752b34245e65133a356dbb4c
danny ong <danny.ong@fdmgroup.com>
2024-02-26 17:01:28 +0800

    autowired BankAccountRepository & BankTransactionRepository to their respective service classes

--- src/main/java/com/fdmgroup/CreditCardProject/service/BankAccountService.java
+++ src/main/java/com/fdmgroup/CreditCardProject/service/BankAccountService.java
@@ -1,8 +1,13 @@
 package com.fdmgroup.CreditCardProject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fdmgroup.CreditCardProject.repository.BankAccountRepository;
 
 @Service
 public class BankAccountService {
 
+	@Autowired
+	private BankAccountRepository bankAccountRepository;
 }
--- src/main/java/com/fdmgroup/CreditCardProject/service/BankTransactionService.java
+++ src/main/java/com/fdmgroup/CreditCardProject/service/BankTransactionService.java
@@ -1,8 +1,13 @@
 package com.fdmgroup.CreditCardProject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fdmgroup.CreditCardProject.repository.BankTransactionRepository;
 
 @Service
 public class BankTransactionService {
-
+	
+	@Autowired
+	private BankTransactionRepository bankTransactionRepository;
 }
