0161648937c35b6dbac9a64fc7821ad67b544b4a
ashwinSarith <ash.sarith@fdmgroup.com>
2024-02-26 10:35:11 +0800

    Push project primer

---
+++ CreditCardProject/src/main/java/com/fdmgroup/CreditCardProject/CreditCardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.CreditCardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardProjectApplication.class, args);
+	}
+
+}
