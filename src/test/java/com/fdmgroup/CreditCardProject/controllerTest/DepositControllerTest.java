e3333baf39ef75de3ce2d1b0f4fecbc322138e89
S.Umar <umar.banafe@fdmgroup.com>
2024-02-27 17:14:31 +0800

    Added tests for deposit controller page

---
+++ src/test/java/com/fdmgroup/CreditCardProject/controllerTest/DepositControllerTest.java
@@ -0,0 +1,33 @@
+package com.fdmgroup.CreditCardProject.controllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import com.fdmgroup.CreditCardProject.controller.DepositController;
+
+class DepositControllerTest {
+
+	@Mock
+	DepositController depositController;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		depositController = new DepositController();
+	}
+
+	@Test
+	void testGoToDepositPage_returnsCorrectString() {
+		String result = depositController.goToDepositPage();
+		assertEquals("deposit", result);
+	}
+	
+	@Test
+	void testGoToDepositPage_doesNotReturnIncorrectString() {
+		String result = depositController.goToDepositPage();
+		assertNotEquals("dashboard", result);
+	}
+
+}
