07e8239e503f2dc84e6d9749bfc2cfb585323f2a
Sam Artigolle <sam.artigolle@fdmgroup.com>
2024-02-28 16:20:14 +0800

    Remove inheritance logic

--- src/main/java/com/fdmgroup/CreditCardProject/model/CreditCard.java
+++ src/main/java/com/fdmgroup/CreditCardProject/model/CreditCard.java
@@ -5,136 +5,231 @@ import java.util.List;
 import jakarta.persistence.*;
 
 /**
- * Represents a credit card account with associated transaction history,
- * payment history, and rewards profile.
+ * Represents a credit card account with associated transaction history, payment
+ * history, and rewards profile.
+ * 
  * @author Danny
  * @version 1.0
  * @since 2024-02-26
  */
 @Entity
 @Table(name = "`Credit Card`")
-public class CreditCard extends Account{
+public class CreditCard {
 
+	/**
+	 * The unique identifier for the account.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "creditCardId")
-	private long creditCardId;
+	private long accountId;
+
 	/**
-     * The due date for monthly payments.
-     */
+	 * The identifier of the owner of the account.
+	 */
+	@Column(name = "ownerId")
+	private long ownerId;
+
+	/**
+	 * The account number associated with this account.
+	 */
+	@Column(name = "accountNumber")
+	private String accountNumber;
+
+	/**
+	 * The current balance of the account.
+	 */
+	@Column(name = "currentBalance")
+	private double currentBalance;
+
+	/**
+	 * The due date for monthly payments.
+	 */
 	@Column(name = "monthlyDueDate")
 	private byte monthlyDueDate;
-	
-	
+
 	/**
-     * The spending limit for the credit card.
-     */
+	 * The spending limit for the credit card.
+	 */
 	@Column(name = "spendingLimit")
 	private long spendingLimit;
-	
+
 	/**
-     * The transaction history of the credit card.
-     */
+	 * The transaction history of the credit card.
+	 */
 	@OneToMany(mappedBy = "creditCard", cascade = CascadeType.ALL)
 	private List<CreditCardTransaction> transactionHistory;
-	
+
 	/**
-     * The payment history of the credit card.
-     */
+	 * The payment history of the credit card.
+	 */
 	@OneToMany(mappedBy = "creditCard", cascade = CascadeType.ALL)
 	private List<BankTransaction> paymentHistory;
-	
+
 	/**
-     * The rewards profile associated with the credit card.
-     */
-	@OneToOne(mappedBy = "creditCard", cascade = CascadeType.ALL)
+	 * The rewards profile associated with the credit card.
+	 */
+	@OneToOne(cascade = CascadeType.ALL)
 	private RewardsProfile rewardProfile;
-	
+
 	/**
-     * Default constructor for creating a CreditCard instance.
-     */
+	 * Default constructor for creating a CreditCard instance.
+	 */
 	public CreditCard() {
 		super();
 	}
 
 	/**
-     * Get the monthly due date for payments.
-     * @return The monthly due date.
-     */
+	 * Get the monthly due date for payments.
+	 * 
+	 * @return The monthly due date.
+	 */
 	public byte getMonthlyDueDate() {
 		return monthlyDueDate;
 	}
 
 	/**
-     * Set the monthly due date for payments.
-     * @param monthlyDueDate The monthly due date to set.
-     */
+	 * Set the monthly due date for payments.
+	 * 
+	 * @param monthlyDueDate The monthly due date to set.
+	 */
 	public void setMonthlyDueDate(byte monthlyDueDate) {
 		this.monthlyDueDate = monthlyDueDate;
 	}
 
 	/**
-     * Get the spending limit for the credit card.
-     * @return The spending limit.
-     */
+	 * Get the spending limit for the credit card.
+	 * 
+	 * @return The spending limit.
+	 */
 	public long getSpendingLimit() {
 		return spendingLimit;
 	}
 
 	/**
-     * Set the spending limit for the credit card.
-     * @param spendingLimit The spending limit to set.
-     */
+	 * Set the spending limit for the credit card.
+	 * 
+	 * @param spendingLimit The spending limit to set.
+	 */
 	public void setSpendingLimit(long spendingLimit) {
 		this.spendingLimit = spendingLimit;
 	}
 
 	/**
-     * Get the transaction history of the credit card.
-     * @return The transaction history.
-     */
+	 * Get the transaction history of the credit card.
+	 * 
+	 * @return The transaction history.
+	 */
 	public List<CreditCardTransaction> getTransactionHistory() {
 		return transactionHistory;
 	}
 
 	/**
-     * Set the transaction history of the credit card.
-     * @param transactionHistory The transaction history to set.
-     */
+	 * Set the transaction history of the credit card.
+	 * 
+	 * @param transactionHistory The transaction history to set.
+	 */
 	public void setTransactionHistory(List<CreditCardTransaction> transactionHistory) {
 		this.transactionHistory = transactionHistory;
 	}
 
 	/**
-     * Get the payment history of the credit card.
-     * @return The payment history.
-     */
+	 * Get the payment history of the credit card.
+	 * 
+	 * @return The payment history.
+	 */
 	public List<BankTransaction> getPaymentHistory() {
 		return paymentHistory;
 	}
 
 	/**
-     * Set the payment history of the credit card.
-     * @param paymentHistory The payment history to set.
-     */
+	 * Set the payment history of the credit card.
+	 * 
+	 * @param paymentHistory The payment history to set.
+	 */
 	public void setPaymentHistory(List<BankTransaction> paymentHistory) {
 		this.paymentHistory = paymentHistory;
 	}
 
 	/**
-     * Get the rewards profile associated with the credit card.
-     * @return The rewards profile.
-     */
+	 * Get the rewards profile associated with the credit card.
+	 * 
+	 * @return The rewards profile.
+	 */
 	public RewardsProfile getRewardProfile() {
 		return rewardProfile;
 	}
 
 	/**
-     * Set the rewards profile associated with the credit card.
-     * @param rewardProfile The rewards profile to set.
-     */
+	 * Set the rewards profile associated with the credit card.
+	 * 
+	 * @param rewardProfile The rewards profile to set.
+	 */
 	public void setRewardProfile(RewardsProfile rewardProfile) {
 		this.rewardProfile = rewardProfile;
+	}
+
+	/**
+	 * Gets the identifier of the owner of the account.
+	 * 
+	 * @return The ownerId.
+	 */
+	public long getOwnerId() {
+		return ownerId;
+	}
+
+	/**
+	 * Sets the identifier of the owner of the account.
+	 * 
+	 * @param ownerId The ownerId to set.
+	 */
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	/**
+	 * Gets the account number associated with this account.
+	 * 
+	 * @return The accountNumber.
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	/**
+	 * Sets the account number associated with this account.
+	 * 
+	 * @param accountNumber The accountNumber to set.
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	/**
+	 * Gets the current balance of the account.
+	 * 
+	 * @return The currentBalance.
+	 */
+	public double getCurrentBalance() {
+		return currentBalance;
+	}
+
+	/**
+	 * Sets the current balance of the account.
+	 * 
+	 * @param currentBalance The currentBalance to set.
+	 */
+	public void setCurrentBalance(double currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+
+	/**
+	 * Gets the unique identifier for the account.
+	 * 
+	 * @return The accountId.
+	 */
+	public long getAccountId() {
+		return accountId;
 	}
 
 }
--- src/main/java/com/fdmgroup/CreditCardProject/model/CreditCardTransaction.java
+++ src/main/java/com/fdmgroup/CreditCardProject/model/CreditCardTransaction.java
@@ -5,9 +5,11 @@ import java.sql.Date;
 import jakarta.persistence.*;
 
 /**
- * Represents a Credit Card Transaction entity.
- * This class extends the Transaction class and contains additional information such as store information,
- * merchant category code, original currency code, and original currency amount.
+ * Represents a Credit Card Transaction entity. This class extends the
+ * Transaction class and contains additional information such as store
+ * information, merchant category code, original currency code, and original
+ * currency amount.
+ * 
  * @author Danny
  * @version 1.0
  * @since 2022-02-20
@@ -15,67 +17,86 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "`Credit Card Transaction`")
-public class CreditCardTransaction extends Transaction{
+public class CreditCardTransaction {
 
+	/**
+	 * The unique identifier for the transaction.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "creditCardTransactionId")
-	private long creditCardTransactionId;
+	@Column(name = "transactionId")
+	private long transactionId;
 
 	/**
-     * The store information related to the credit card transaction.
-     */
+	 * The credit card associated with this transaction.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "creditCardId")
+	private CreditCard creditCard;
+
+	/**
+	 * The date of the transaction.
+	 */
+	@Column(name = "date")
+	private Date date;
+
+	/**
+	 * The amount involved in the transaction.
+	 */
+	@Column(name = "amount")
+	private double amount;
+
+	/**
+	 * The store information related to the credit card transaction.
+	 */
 	@Column(name = "storeInfo")
 	private String storeInfo;
-	
+
 	/**
-     * The merchant category code associated with the credit card transaction.
-     */
+	 * The merchant category code associated with the credit card transaction.
+	 */
 	@ManyToOne
-    @JoinColumn(name = "merchantCategoryCodeId")
+	@JoinColumn(name = "merchantCategoryCodeId")
 	private MerchantCategoryCode merchantCategoryCode;
-	
+
 	/**
-     * The original currency code of the credit card transaction.
-     */
+	 * The original currency code of the credit card transaction.
+	 */
 	@Column(name = "originalCurrencyCode")
 	private String originalCurrencyCode;
-	
+
 	/**
-     * The original currency amount of the credit card transaction.
-     */
+	 * The original currency amount of the credit card transaction.
+	 */
 	@Column(name = "originalCurrencyAmount")
 	private double originalCurrencyAmount;
-	
+
 	/**
-     * The credit card associated with this transaction.
-     */
-    @ManyToOne
-    @JoinColumn(name = "creditCardId")
-    private CreditCard creditCard;
-	
-	/**
-     * Default constructor for CreditCardTransaction.
-     * Creates an instance with default values.
-     */
+	 * Default constructor for CreditCardTransaction. Creates an instance with
+	 * default values.
+	 */
 	public CreditCardTransaction() {
 		super();
 	}
-	
-	 /**
-     * Parameterized constructor for CreditCardTransaction.
-     *
-     * @param accountFromId            The account ID associated with the transaction.
-     * @param date                     The date of the transaction.
-     * @param amount                   The amount of the transaction.
-     * @param storeInfo                The store information related to the transaction.
-     * @param merchantCategoryCode     The merchant category code associated with the transaction.
-     * @param originalCurrencyCode     The original currency code of the transaction.
-     * @param originalCurrencyAmount   The original currency amount of the transaction.
-     */
-	public CreditCardTransaction(long accountFromId, Date date, double amount, String storeInfo, MerchantCategoryCode merchantCategoryCode,String originalCurrency, double originalCurrencyAmount) {
+
+	/**
+	 * Parameterized constructor for CreditCardTransaction.
+	 *
+	 * @param accountFromId          The account ID associated with the transaction.
+	 * @param date                   The date of the transaction.
+	 * @param amount                 The amount of the transaction.
+	 * @param storeInfo              The store information related to the
+	 *                               transaction.
+	 * @param merchantCategoryCode   The merchant category code associated with the
+	 *                               transaction.
+	 * @param originalCurrencyCode   The original currency code of the transaction.
+	 * @param originalCurrencyAmount The original currency amount of the
+	 *                               transaction.
+	 */
+	public CreditCardTransaction(CreditCard creditCard, Date date, double amount, String storeInfo,
+			MerchantCategoryCode merchantCategoryCode, String originalCurrency, double originalCurrencyAmount) {
 		super();
-		setAccountFromId(accountFromId);
+		setCreditCard(creditCard);
 		setDate(date);
 		setAmount(amount);
 		setStoreInfo(storeInfo);
@@ -84,69 +105,124 @@ public class CreditCardTransaction extends Transaction{
 		setOriginalCurrencyAmount(originalCurrencyAmount);
 	}
 
-	 /**
-     * Gets the store information of the credit card transaction.
-     * @return The storeInfo.
-     */
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	/**
+	 * Gets the store information of the credit card transaction.
+	 * 
+	 * @return The storeInfo.
+	 */
 	public String getStoreInfo() {
 		return storeInfo;
 	}
 
 	/**
-     * Sets the store information of the credit card transaction.
-     * @param storeInfo The store information to set.
-     */
+	 * Sets the store information of the credit card transaction.
+	 * 
+	 * @param storeInfo The store information to set.
+	 */
 	public void setStoreInfo(String storeInfo) {
 		this.storeInfo = storeInfo;
 	}
 
 	/**
-     * Gets the merchant category code associated with the credit card transaction.
-     * @return The merchantCategoryCode.
-     */
+	 * Gets the merchant category code associated with the credit card transaction.
+	 * 
+	 * @return The merchantCategoryCode.
+	 */
 	public MerchantCategoryCode getMerchantCategoryCode() {
 		return merchantCategoryCode;
 	}
 
 	/**
-     * Sets the merchant category code associated with the credit card transaction.
-     * @param merchantCategoryCode The merchant category code to set.
-     */
+	 * Sets the merchant category code associated with the credit card transaction.
+	 * 
+	 * @param merchantCategoryCode The merchant category code to set.
+	 */
 	public void setMerchantCategoryCode(MerchantCategoryCode merchantCategoryCode) {
 		this.merchantCategoryCode = merchantCategoryCode;
 	}
 
 	/**
-     * Gets the original currency code of the credit card transaction.
-     * @return The originalCurrencyCode.
-     */
+	 * Gets the original currency code of the credit card transaction.
+	 * 
+	 * @return The originalCurrencyCode.
+	 */
 	public String getOriginalCurrencyCode() {
 		return originalCurrencyCode;
 	}
 
 	/**
-     * Sets the original currency code of the credit card transaction.
-     * @param originalCurrencyCode The original currency code to set.
-     */
+	 * Sets the original currency code of the credit card transaction.
+	 * 
+	 * @param originalCurrencyCode The original currency code to set.
+	 */
 	public void setOriginalCurrencyCode(String originalCurrencyCode) {
 		this.originalCurrencyCode = originalCurrencyCode;
 	}
 
 	/**
-     * Gets the original currency amount of the credit card transaction.
-     * @return The originalCurrencyAmount.
-     */
+	 * Gets the original currency amount of the credit card transaction.
+	 * 
+	 * @return The originalCurrencyAmount.
+	 */
 	public double getOriginalCurrencyAmount() {
 		return originalCurrencyAmount;
 	}
 
 	/**
-     * Sets the original currency amount of the credit card transaction.
-     * @param originalCurrencyAmount The original currency amount to set.
-     */
+	 * Sets the original currency amount of the credit card transaction.
+	 * 
+	 * @param originalCurrencyAmount The original currency amount to set.
+	 */
 	public void setOriginalCurrencyAmount(double originalCurrencyAmount) {
 		this.originalCurrencyAmount = originalCurrencyAmount;
 	}
-	
-	
+
+	public long getTransactionId() {
+		return transactionId;
+	}
+
+	/**
+	 * Gets the date of the transaction.
+	 * 
+	 * @return The date.
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * Sets the date of the transaction.
+	 * 
+	 * @param date The date to set.
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * Gets the amount involved in the transaction.
+	 * 
+	 * @return The amount.
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * Sets the amount involved in the transaction.
+	 * 
+	 * @param amount The amount to set.
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
 }
