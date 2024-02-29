3d2990ef2b82fd91e4a3d6df7183e1b5b32e0c1c
danny ong <danny.ong@fdmgroup.com>
2024-02-29 22:23:57 +0800

    reverted changes back to merging with eric's branch

--- src/main/java/com/fdmgroup/CreditCardProject/controller/DashboardController.java
+++ src/main/java/com/fdmgroup/CreditCardProject/controller/DashboardController.java
@@ -11,10 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.fdmgroup.CreditCardProject.service.BankAccountService;
-
-import java.util.List;
-
 @Controller
 
 public class DashboardController {
@@ -22,16 +18,11 @@ public class DashboardController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private BankAccountService bankAccountService;
-  
-  	@GetMapping("/dashboard")
-  	public String goToDashboard(@AuthenticationPrincipal AuthUser principal,Model model) {
-  		System.out.println(principal.getUsername()); //to get username of current user for future use
-  		String username = principal.getUsername();
-  		User user = userService.getUserByUsername(username);
-  		List<String> bankAccountIds = bankAccountService.getBankAccountIdsByUsername(user.getUsername());
-  		model.addAttribute("bankAccountIds", bankAccountIds);
-  		return "dashboard";
-  	}
+    @GetMapping("/dashboard")
+    public String goToDashboard(@AuthenticationPrincipal AuthUser principal, Model model) {
+        User currentUser = userService.getUserByUsername(principal.getUsername());
+        model.addAttribute("user", currentUser);
+        return "dashboard";
+    }
+
 }
