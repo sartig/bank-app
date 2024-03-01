2b0f2a18d66164d1274f99a0a7b5b8dfa4d07f53
teckleong.lim <teckleong.lim@fdmgroup.com>
2024-03-01 09:58:31 +0800

    add credit card html and css

---
+++ src/main/java/com/fdmgroup/CreditCardProject/controller/CreditCardController.java
@@ -0,0 +1,25 @@
+package com.fdmgroup.CreditCardProject.controller;
+
+import com.fdmgroup.CreditCardProject.model.AuthUser;
+import com.fdmgroup.CreditCardProject.model.User;
+import com.fdmgroup.CreditCardProject.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class CreditCardController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/creditcard")
+    public String goToDashboard(@AuthenticationPrincipal AuthUser principal, Model model) {
+        User currentUser = userService.getUserByUsername(principal.getUsername());
+        model.addAttribute("user", currentUser);
+        return "creditcard";
+    }
+}
